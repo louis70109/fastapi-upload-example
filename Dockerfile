6d72f25cc843caea79b36420d3b16785f980f8f5
louis70109 <louis70109@gmail.com>
2021-04-09 12:51:27 +0800

    add test case sample and main function

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM python:3.7
 
 COPY requirements.txt /requirements.txt
-
 RUN pip install -r requirements.txt
 
 WORKDIR /app
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import uvicorn
+
 from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 from routers import users, items
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 app.include_router(users.router)
@@ -15,9 +15,13 @@ app.include_router(items.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Bigger Applications!"}
+    return {"message": "Hello World!"}
 
 
 @app.get("/liff", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
