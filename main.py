cec0a91a36e0010a18e1f998889bd7ad917f6398
louis70109 <louis70109@gmail.com>
2022-09-23 19:14:52 +0800

    ✨feat: add file upload GET/POST page

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 import uvicorn
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi import File, UploadFile
+from typing import List
 
 from routers import users, items
 
@@ -14,14 +17,30 @@ app.include_router(items.router)
 
 
 @app.get("/")
-async def root():
+async def health():
     return {"message": "Hello World!"}
 
 
-@app.get("/liff", response_class=HTMLResponse)
-async def read_item(request: Request):
+@app.get("/upload", response_class=HTMLResponse)
+async def upload_page(request: Request):
+
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload")
+def upload(files: List[UploadFile] = File(...)):
+    for file in files:
+        try:
+            contents = file.file.read()
+            with open(file.filename, 'wb') as f:
+                f.write(contents)
+        except Exception:
+            return {"message": "There was an error uploading the file(s)"}
+        finally:
+            file.file.close()
+
+    return {"message": "Successfully uploaded"}  
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    port = os.getenv('PORT', default=5000)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
