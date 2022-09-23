86363b1938d2975e0878d247cc484182f5f8e0c7
louis70109 <louis70109@gmail.com>
2022-09-23 19:14:32 +0800

    ✨feat: add file upload test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,6 @@
+import io
+import os
+from pathlib import Path
 import unittest
 from _pytest.monkeypatch import MonkeyPatch
 from mock import patch
@@ -8,10 +11,26 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main2():
+def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World!"}
+
+def test_upload():
+    filename = 'requirements.txt'
+    try:
+        with open(filename, 'rb') as f:
+
+            text = f.read()
+            response = client.post("/upload", files={"files": (filename, text)})
+            assert response.status_code == 200
+            assert response.json() == {"message": "Successfully uploaded"}
+    except:
+        raise
+    # data = {'files': [(io.StringIO('my file contents'), 'hello world.txt')]}
+    # print(data)
+    
+    # assert response.json() == {"message": "Hello World!"}
 
 # class TestClient(unittest.TestCase):
 #     def setUp(self):
